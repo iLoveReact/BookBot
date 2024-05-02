0ffb1ac0720a9014f64edde847eb45b3f118f7e5
iLoveReact <maksym.slotwinski2006@gmail.com>
2024-05-02 22:41:40 +0200

    Create an agent

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+from langchain_core.tools import Tool, StructuredTool
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain import hub
+from langchain.agents import AgentExecutor, create_react_agent
+from tools.tools import get_books_pdf_paths
 
 load_dotenv()
 
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
+tools = [StructuredTool.from_function(
+    func=get_books_pdf_paths,
+    description="Provides all book paths. Can be used to retrieve a path of specific book",
+    name="get book pdf paths"
+)]
+prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(llm=llm, prompt=prompt, tools=tools)
+agent_executor = AgentExecutor(agent=agent, tools=tools)
+book_path = agent_executor.invoke({"input": "What is the path to pdf of a book called Old Man And The Sea?"})
+print(book_path)
 
--- tools/tools.py
+++ tools/tools.py
@@ -1,8 +1,11 @@
 from typing import List
 import os
 
-def get_books_pdf_paths() -> List[str]:
-    """Provides all book paths. Can be used to retrieve a path of specific book"""
-    book_paths = ["book/" + book for book in os.listdir("../books")]
+from langchain_core.tools import tool
+
+
+def get_books_pdf_paths(name: str = "") -> List[str]:
+    """Provides all book paths. Can be used to retrieve a path of specific book, does't take any arguments"""
+    book_paths = ["books/" + book for book in os.listdir("C:/Users/Asus/Desktop/books_app/books")]
 
     return book_paths
